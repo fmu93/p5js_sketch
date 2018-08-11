389e461fa03c376145ba7286e0a2ba6c208de7d4
fmu93 <fco.muro2@gmail.com>
2018-08-11 21:43:45 +0200

    crating new fireworks2 project

---
+++ fireworks2/Particle.js
@@ -0,0 +1,67 @@
+
+class Particle {
+  
+  constructor() {
+    this.pos = createVector(mouseX, mouseY);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    
+    this.explosion = 2;
+    this.c = color(255, 100 + 150*random(1), 150 + random(20, 100));
+    this.spikes = 4 + Math.floor(random(4));
+    this.lifeSpan = randomGaussian(60, 40) + 250;
+    this.size = this.lifeSpan*0.1;
+    this.mass = this.size/50; 
+  }
+  
+  isDead() {
+    if (this.lifeSpan <= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  applyForce(force) {
+    var f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
+  
+  update() {
+    this.fade();
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    this.lifeSpan -= 0.5 + 2*noise(this.lifeSpan*this.spikes);
+  }
+  
+  fade() {
+    this.size = this.lifeSpan*0.2;
+    this.c = color(red(this.c), green(this.c), blue(this.c), alpha(this.c) - 0.5)
+    //mass = size/200; // actually more realistic but small bits fly too fast
+  }
+  
+  display() {
+    noStroke();
+    fill(this.c);
+    this.star(this.pos.x, this.pos.y, this.size, this.size/3, this.spikes);
+  }
+  
+  star(x, y, radius1, radius2, npoints) {
+    var angle = TWO_PI / npoints;
+    var halfAngle = angle/2.0;
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += angle) {
+      var sx = x + cos(a) * radius2;
+      var sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a+halfAngle) * radius1;
+      sy = y + sin(a+halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+  
+  
+  
+}
---
+++ fireworks2/ParticleSystem.js
@@ -0,0 +1,64 @@
+class ParticleSystem {
+  
+  constructor(n) {
+
+    this.gravity = createVector(0, 0.008);
+    this.exMean = 2;
+    this.exStDev = 0.4;
+    this.dragCoeff = 0.002;
+    this.particles = [];
+    for(var i = 0; i < n; i++) {
+      var p = new Particle();
+      this.explosion(p);
+      this.particles.push(p);
+    }
+  }
+  
+  explosion(p) {
+    var ex = createVector(randomGaussian(this.exMean, this.exStDev), 0);
+    ex.rotate(random(TWO_PI));
+    p.applyForce(ex);
+  }
+
+  drag(p) {
+    // drag
+    var drag = p.vel.copy();
+    drag.normalize();
+    drag.mult(-1 * this.dragCoeff * p.vel.magSq() * p.mass);
+    p.applyForce(drag);
+  }
+
+  gravityApply(p) {
+    // gravity force
+    var g = this.gravity.copy();
+    g.mult(p.mass);
+    p.applyForce(g);
+  }
+  
+  run() {
+   for (var i = this.particles.length - 1; i >= 0; i--) {
+    var p = this.particles[i];
+    this.gravityApply(p);
+    // drag force
+    this.drag(p);
+    // update
+    p.update();
+    p.display();
+    
+    if (p.isDead()) {
+     this.particles.splice(i, 1);
+    }
+   }  
+  }
+  
+  isDead() {
+   if (this.particles.length == 0) {
+    return true; 
+   } else {
+     return false;
+   }
+  }
+  
+  
+  
+}
