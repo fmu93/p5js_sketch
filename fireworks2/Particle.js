e33b19325fc985df13db689684ffbeaddc3a1380
fmu93 <fco.muro2@gmail.com>
2018-08-12 04:44:16 +0200

    fixed size on fireworks2

--- fireworks2/Particle.js
+++ fireworks2/Particle.js
@@ -6,12 +6,13 @@ class Particle {
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
     
+    this.zoom = (width+height)*0.00007;
     this.explosion = 2;
     this.c = color(255, 100 + 150*random(1), 150 + random(20, 100));
     this.spikes = 4 + Math.floor(random(4));
     this.lifeSpan = randomGaussian(60, 40) + 250;
-    this.size = this.lifeSpan*0.1;
-    this.mass = this.size/50; 
+     this.size = this.lifeSpan*this.zoom;
+    this.mass = this.lifeSpan*0.002; 
   }
   
   isDead() {
@@ -36,7 +37,7 @@ class Particle {
   }
   
   fade() {
-    this.size = this.lifeSpan*0.2;
+    this.size = this.lifeSpan*this.zoom;
     this.c = color(red(this.c), green(this.c), blue(this.c), alpha(this.c) - 0.5)
     //mass = size/200; // actually more realistic but small bits fly too fast
   }
