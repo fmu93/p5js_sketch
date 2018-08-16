9e358c7f39e777a966eb6fa2ee12a2ce09347f7d
fmu93 <fco.muro2@gmail.com>
2018-08-17 00:14:13 +0200

    messing with physics

--- bubble_gum/bubble_gum.js
+++ bubble_gum/bubble_gum.js
@@ -1,37 +1,31 @@
 
 // module aliases
 var Engine = Matter.Engine,
-    // Render = Matter.Render, // don't need it
-    World = Matter.World,
-    Bodies = Matter.Bodies;
+// Render = Matter.Render, // don't need it
+World = Matter.World,
+Bodies = Matter.Bodies;
 
-    // create an engine
+// create an engine			
 var engine;
 var world;
 
-var n; 
+var n = 120; 
 var bubbles = [];
 var forceCoeff = 0.000002
+var back = 0;
 
-var k;
-var back;
-var particles;
+var blendModes = ['BLEND', 'DARKEST', 'LIGHTEST', 'DIFFERENCE', 'MULTIPLY', 'EXCLUSION', 'SCREEN', 'REPLACE', 'OVERLAY', 'HARD_LIGHT', 'SOFT_LIGHT', 'DODGE', 'BURN', 'ADD', 'NORMAL'];
+var b = 0;
 
 function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
 	world.gravity.y = 0;
-
-	k = 0;
-	n = 100;
-	back = 0;	
 	particles = [];
-	pixelate = 8;
-	pixelateOn = false;
 
   createCanvas(windowWidth, windowHeight);           // set size to that of the image
-  // colorMode(HSB, 255);               // allows us to access the brightness of a color
+  colorMode(HSB, 255);               // allows us to access the brightness of a color
   background(back);
 
   // Start particles
@@ -46,7 +40,7 @@ function setup() {
 }
 
 function draw() {
-	// blendMode(LIGHTEST);
+	// blendMode(NORMAL);
 	background(back);
 
 	// draw circles
@@ -54,122 +48,20 @@ function draw() {
 		var bubble = bubbles[i];
 		var from = Matter.Vector.create(mouseX, mouseY);
 		var force = Matter.Vector.create(-(bubble.body.position.x - mouseX)*forceCoeff, -(bubble.body.position.y - mouseY)*forceCoeff);
+		Matter.Vector.mult(force, bubble.body.mass);
 		Matter.Body.applyForce(bubble.body, from, force);
 		bubble.show();
 	}
-
-
-	// deal with each particle
-	// for (var i=0; i < particles.length; i++) {
-	// 	particles[i].wobble();
-	// 	particles[i].pointer();
- //   		// reaction among particles
-	//     for (var j=i+1; j < particles.length; j++) {
-	//    		particles[i].checkCollisionOther(particles[j]);
-	//     }
-	// 	particles[i].move();
-	//     particles[i].walls();
-	//    	particles[i].display(); 
-	// }
-
-  // influence area
-  // noFill();
-  // stroke(back, 80);
-  // ellipse(mouseX, mouseY, height/2, height/2);
   
-  // frame
-  // strokeWeight(30);
-  // stroke(back);
-  // noFill();
-  // rect(0,0,width,height);
-  // strokeWeight(1);
+}
 
-  // show framerate
-  if ((k+1) % 50 == 0){
-  	var fps = frameRate();
-  	fill(255);
-  	stroke(0);
-  	textSize(12);
-  	text("FPS: " + fps.toFixed(0), 10, height - 10);
-  }
-  k++;
-
- //  	// clicl me! text
-	// fill(255);
- //  	stroke(0);
- //  	textSize(16);
- //  	text("Click around!", 20, 12);
-  
+function mousePressed() {
+	forceCoeff *= -1;
+	console.log(b);
+	b = (b+1)/blendModes.lenght;
 }
 
 function mouseDragged() {
-
-	  	var bubble = new Bubble(mouseX, mouseY);
-  	bubbles.push(bubble);
-
-	for (var i=0; i < particles.length; i++) {
-		particles[i].s = particles[i].s * -1;
-	}
-}
-
-
-class Particle {
-
-	constructor() {
-		this.c = color(random(100, 255), random(100, 255), random(100, 255));
-		this.pos = createVector(random(width), random(height));
-		this.speed = createVector(0,0);
-		this.size = int(width*random(0.01,0.03));
-		this.friction = 0.967; 
-		this.s = 0.08; // strength of pull
-		this.w = 0.1; // wobblyness
-		this.RInfluence = height/4; // radius of influence of force
-	}
-
-	checkCollisionOther(other) {
-		var distV = p5.Vector.sub(this.pos, other.pos);
-		if (distV.mag() < (other.size + this.size)/2.0) {
-			var repel = distV.normalize().mult(this.w);
-			this.speed.add(repel);
-			other.speed.sub(repel);
-		}
-	}
-
-	wobble() {
-		this.speed.add(random(-this.w,this.w), random(-this.w,this.w));
-	}
-
-	pointer() {
-		var mouse = createVector(mouseX, mouseY);
-		var diff = mouse.sub(this.pos);
-		if (this.RInfluence/diff.mag() >= 1) {
-			//speed.add(diff.normalize().mult(s*(1/pow(diff.mag(), 1)))); 
-			this.speed.add(diff.normalize().mult( this.s * pow(1 - diff.mag()/this.RInfluence, 0.5) )); 
-	  	} 
-	}
-
-	move() {
-	  this.pos.add(this.speed);
-	  this.speed.mult(this.friction);   
-	}
-
-	walls() {
-		if (this.pos.x > width) {
-			this.pos.x = 0; 
-		} else if (this.pos.x < 0) {
-			this.pos.x = width;
-		}
-		if (this.pos.y > height) {
-			this.pos.y = 0;
-		} else if (this.pos.y < 0) {
-			this.pos.y = height;
-		}
-	}
-
-	display() {
-		stroke(this.c);
-		fill(this.c);
-		ellipse(this.pos.x, this.pos.y, this.size, this.size);
-	}
-
+	// var bubble = new Bubble(mouseX, mouseY);
+ //  	bubbles.push(bubble);
 }
