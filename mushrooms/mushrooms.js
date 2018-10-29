a88f03b326b933b62cd12132d6d6fe697b462f7d
fmu93 <com>
2018-10-29 15:43:04 +0100

    fullscreen

--- mushrooms/mushrooms.js
+++ mushrooms/mushrooms.js
@@ -2,7 +2,7 @@ var mList = [];
 var count = 16;
 
 function setup() {
-    createCanvas(1000, 600);
+    createCanvas(windowWidth, windowHeight);
     for (var i = 0; i < count; i++) {
         mList.push(new Mushroom());
     }
@@ -22,13 +22,13 @@ class Mushroom {
         this.r = random(80, 120);
         this.maxBrightness = 255;
         this.time = 0;
-        this.glowSpeed = 0.1;
+        this.glowSpeed = random(0.1, 0.3);
         this.timeDot = 0.01;
 
     }
 
     display() {
-        var f = this.maxBrightness * noise(this.time, this.pos.x, this.pos.y) * (0.2 + 0.8*sin(this.time * this.glowSpeed));
+        var f = this.maxBrightness * noise(this.time, this.pos.x, this.pos.y) * (0.2 + 0.8*sin(this.time * this.glowSpeed * noise(this.time)));
         fill(f);
         ellipse(this.pos.x, this.pos.y, this.r, this.r);
         this.time += this.timeDot;
