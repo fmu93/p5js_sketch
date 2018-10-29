51bf05474f606ce3a6de371f1d0e918cb35cbba3
fmu93 <com>
2018-10-29 15:27:04 +0100

    mushrooms

---
+++ mushrooms/mushrooms.js
@@ -0,0 +1,36 @@
+var mList = [];
+var count = 10;
+
+function setup() {
+    createCanvas(1000, 600);
+    for (var i = 0; i < count; i++) {
+        mList.push(new Mushroom());
+    }
+}
+
+function draw() {
+    background(255);
+
+    for (var i = 0; i < mList.length; i++) {
+        mList[i].display();
+    }
+}
+
+class Mushroom {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.r = random(80, 200);
+        this.maxBrightness = 255;
+        this.time = 0;
+        this.glowSpeed = 1;
+        this.timeDot = 0.01;
+
+    }
+
+    display() {
+        var f = this.maxBrightness * noise(this.time, this.pos.x, this.pos.y) * sin(this.time * this.glowSpeed);
+        fill(f);
+        ellipse(this.pos.x, this.pos.y, this.r, this.r);
+        this.time += this.timeDot;
+    }
+}
