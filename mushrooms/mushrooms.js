38189c52671f878775d296f25632ef8f36ae2b7b
fmu93 <fco.muro2@gmail.com>
2018-10-29 23:06:44 +0100

    texthint

--- mushrooms/mushrooms.js
+++ mushrooms/mushrooms.js
@@ -1,11 +1,16 @@
 var mList = [];
-var count = 400;
+var count = 200;
+
+var hintText = "<click to add mushrooms>\n<spacebar to switch add mode>";
+
+var mode = 0;
 var time = 0;
 var timeDot = 0.01;
 var myShroom;
-var spatialNoiseFactor = 0.004;
+var spatialNoiseFactor = 0.003;
 var std = 5;
 var mean = 20;
+var sineFactor = 0.35
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -14,10 +19,19 @@ function setup() {
     }
     myShroom = new Mushroom();
     noCursor();
+
 }
 
 function draw() {
     background(0);
+
+    // hint text
+    fill(120, 120);
+    textAlign(CENTER);
+    rectMode(CENTER);
+    textSize(60);
+    text(hintText, width / 2, 50);
+
     noStroke();
     for (var i = 0; i < mList.length; i++) {
         mList[i].display();
@@ -28,18 +42,39 @@ function draw() {
     time += timeDot;
 }
 
+function mouseDragged() {
+    if (mode == 0) {
+        mList.push(myShroom);
+        myShroom = new Mushroom();
+    }
+}
+
+function mouseClicked() {
+    if (mode == 1) {
+        mList.push(myShroom);
+        myShroom = new Mushroom();
+    }
+}
+
+function keyPressed() {
+    if (key == ' ') {
+        mode = (mode + 1) % 2;
+        hintText = "<mode " + mode.toString() + ">";
+    }
+}
+
 class Mushroom {
     constructor() {
         this.r = randomGaussian(mean, std);
         this.pos = createVector(random(this.r/2, width - this.r/2), random(this.r/2, height - this.r/2));
-        this.maxBrightness = randomGaussian(240);
-        this.minBrightness = randomGaussian(0, 10);
-        this.glowSpeed = randomGaussian(1, 0.1);
+        this.maxBrightness = randomGaussian(255);
+        this.minBrightness = 0;
+        this.glowSpeed = randomGaussian(1, 1);
     }
 
     display() {
         var f = this.minBrightness + this.maxBrightness * noise(time, this.pos.x*spatialNoiseFactor, this.pos.y*spatialNoiseFactor)
-        * (0.6 + 0.4*sin(time * this.glowSpeed));
+        * (1 - sineFactor*sin(time * this.glowSpeed));
         fill(f);
         ellipse(this.pos.x, this.pos.y, this.r, this.r);
     }
