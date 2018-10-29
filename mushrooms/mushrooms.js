7d6e0edc1f60bad847890bb17747521b33542ee7
fmu93 <fco.muro2@gmail.com>
2018-10-29 22:24:38 +0100

    many small mushrooms

--- mushrooms/mushrooms.js
+++ mushrooms/mushrooms.js
@@ -1,36 +1,46 @@
 var mList = [];
-var count = 16;
+var count = 400;
+var time = 0;
+var timeDot = 0.01;
+var myShroom;
+var spatialNoiseFactor = 0.004;
+var std = 5;
+var mean = 20;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     for (var i = 0; i < count; i++) {
         mList.push(new Mushroom());
     }
+    myShroom = new Mushroom();
+    noCursor();
 }
 
 function draw() {
-    background(255);
-
+    background(0);
+    noStroke();
     for (var i = 0; i < mList.length; i++) {
         mList[i].display();
     }
+    myShroom.pos = createVector(mouseX, mouseY);
+    myShroom.display();
+
+    time += timeDot;
 }
 
 class Mushroom {
     constructor() {
-        this.pos = createVector(random(width), random(height));
-        this.r = random(80, 120);
-        this.maxBrightness = 255;
-        this.time = 0;
-        this.glowSpeed = random(0.1, 0.3);
-        this.timeDot = 0.01;
-
+        this.r = randomGaussian(mean, std);
+        this.pos = createVector(random(this.r/2, width - this.r/2), random(this.r/2, height - this.r/2));
+        this.maxBrightness = randomGaussian(240);
+        this.minBrightness = randomGaussian(0, 10);
+        this.glowSpeed = randomGaussian(1, 0.1);
     }
 
     display() {
-        var f = this.maxBrightness * noise(this.time, this.pos.x, this.pos.y) * (0.2 + 0.8*sin(this.time * this.glowSpeed * noise(this.time)));
+        var f = this.minBrightness + this.maxBrightness * noise(time, this.pos.x*spatialNoiseFactor, this.pos.y*spatialNoiseFactor)
+        * (0.6 + 0.4*sin(time * this.glowSpeed));
         fill(f);
         ellipse(this.pos.x, this.pos.y, this.r, this.r);
-        this.time += this.timeDot;
     }
 }
